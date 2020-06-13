523c81cac5d684a3e9b726a7d69b58fa9e7bf494
Hans Huebner <hans.huebner@gmail.com>
2020-06-13 15:10:27 +0200

    Remove unused option parsing code for now

--- src/index.js
+++ src/index.js
@@ -7,22 +7,7 @@ import { file } from 'tmp';
 
 import { getOptions } from 'loader-utils';
 
-import validateOptions from 'schema-utils';
-
-const schema = {
-  type: 'object',
-  properties: {
-    test: {
-      type: 'string'
-    }
-  }
-};
-
 export default function(source) {
-    const options = getOptions(this);
-
-    validateOptions(schema, options, 'XSLT3 Loader');
-
     const callback = this.async();
     file((err1, xsltPath, fd) => {
         if (err1) throw err1;
@@ -30,7 +15,7 @@ export default function(source) {
         file((err2, sefPath) => {
             if (err2) throw err2;
             const script = `xslt3 -t -nogo -xsl:${xsltPath} -export:${sefPath}`;
-            const command = exec(script, options,
+            const command = exec(script, {},
                                  (err) => {
                                      if (err) return callback(err);
                                      const sef = readFileSync(sefPath);
